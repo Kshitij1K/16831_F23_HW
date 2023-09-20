11aba07f35c90941c76a6dce38250084aa5801f0
Kshitij Kabeer <kshitijkabeer@gmail.com>
2023-09-20 11:05:02 -0400

    Add sample random data function

--- hw1/rob831/infrastructure/replay_buffer.py
+++ hw1/rob831/infrastructure/replay_buffer.py
@@ -76,8 +76,9 @@ class ReplayBuffer(object):
         ## HINT 1: use np.random.permutation to sample random indices
         ## HINT 2: return corresponding data points from each array (i.e., not different indices from each array)
         ## HINT 3: look at the sample_recent_data function below
+        idxs_to_pick = np.random.permutation(batch_size)
 
-        return TODO, TODO, TODO, TODO, TODO
+        return self.obs[idxs_to_pick], self.acs[idxs_to_pick], self.rews[idxs_to_pick], self.next_obs[idxs_to_pick], self.terminals[idxs_to_pick]
 
 
     def sample_recent_data(self, batch_size=1):

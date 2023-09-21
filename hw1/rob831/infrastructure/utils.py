78e48c85fc6218c5071764b69393c8ebdd8724c1
Kshitij Kabeer <kshitijkabeer@gmail.com>
2023-09-21 02:20:10 -0400

    Implement sample trajectory(still not complete)

--- hw1/rob831/infrastructure/utils.py
+++ hw1/rob831/infrastructure/utils.py
@@ -7,8 +7,8 @@ import time
 def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('rgb_array')):
 
     # initialize env for the beginning of a new rollout
-    ob = TODO  # HINT: should be the output of resetting the env [OK]
-
+    ob = env.reset()  # HINT: should be the output of resetting the env [OK]
+    
     # init vars
     obs, acs, rewards, next_obs, terminals, image_obs = [], [], [], [], [], []
     steps = 0
@@ -27,11 +27,12 @@ def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('
 
         # use the most recent ob to decide what to do
         obs.append(ob)
-        ac = TODO # HINT: query the policy's get_action function [OK]
+        ac = policy.get_action(ob) # HINT: query the policy's get_action function [OK]
         ac = ac[0]
         acs.append(ac)
 
         # take that action and record results
+        print(ac)
         ob, rew, done, _ = env.step(ac)
 
         # record result of taking that action
@@ -41,7 +42,7 @@ def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('
 
         # TODO end the rollout if the rollout ended
         # HINT: rollout can end due to done, or due to max_path_length
-        rollout_done = TODO  # HINT: this is either 0 or 1
+        rollout_done = (steps >= max_path_length) or done  # HINT: this is either 0 or 1
         terminals.append(rollout_done)
 
         if rollout_done:
@@ -60,8 +61,8 @@ def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, r
     timesteps_this_batch = 0
     paths = []
     while timesteps_this_batch < min_timesteps_per_batch:
-
-        TODO
+        paths.append(sample_trajectory(env, policy, max_path_length, render, render_mode))
+        timesteps_this_batch += get_pathlength(paths[-1])
 
     return paths, timesteps_this_batch
 
